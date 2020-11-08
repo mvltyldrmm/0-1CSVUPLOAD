2cca1ac848fcff21688495afae2068e4fde6e036
Mevlüt Yıldırım <mevlut.yildirim84@erzurum.edu.tr>
2020-11-08 13:56:50 +0300

    guncelleme

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import pymongo
 import datetime
 import json
+import random
 from veritabani import * #.gitignore
 app = Flask(__name__)
 
@@ -15,14 +16,6 @@ mycollection = db["collectionname"]
 people = db.people
 veri1 = db.veri1
 
-# personDocument = {
-#   "name": { "first": "Alan", "last": "Turing" },
-#   "birth": datetime.datetime(1912, 6, 23),
-#   "death": datetime.datetime(1954, 6, 7),
-#   "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-#   "views": 1250000
-# }
-# people.insert_one(personDocument)
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -30,84 +23,37 @@ def index():
 
 @app.route('/data',methods=['GET','POST'])
 def data():
+
+    uretilen_sayi = randomUret()
+    
     if request.method == 'POST':
         f = request.form['csvfile']
-        # csvler.insert_one(f)
         data = []
         sozluk_data = {}
         with open(f) as file:
             csvfile = csv.reader(file)
             for row in csvfile:
                 data.append(row)
+
         with open(f,encoding='utf-8') as file2:
             csvReader = csv.DictReader(file2)
             for rows in csvReader:
   
-                key = rows['csvno']
-                sozluk_data[key] = rows
+
+                rows["csvno"] = uretilen_sayi
                 veri1.insert_one(rows)
                 
         data_frame = pd.DataFrame(data)
-        print(type(sozluk_data))
 
 
-        # data_json = json.dumps(data)
-        # #data_json = data.json()
-        # #veri1.insert_one(data)
-        # veri1.insert(sozluk_data)
-        return render_template("data.html", data = data_frame.to_html(header=False, index= False))
+        return render_template("data.html", data = data_frame.to_html(header=False, index= False), uretilen = uretilen_sayi)
 @app.route('/secim',methods=['GET','POST'])
 def secim():
     return render_template("goruntule.html")
 @app.route('/goruntule',methods=['GET','POST'])
 def goruntule():
     
-    # #print(people.find_one({ "name.last": "Turing" }))
-    # #asddas = veri1({ "2.spor": "basketbol"})
-    # print(veri1.find_one({ "2.spor": "basketbol"}))
-
-    # scotts_posts = veri1.find_one({'1.spor': 'futbol'})
-    # asdasd = people.find_one({ "name.last": "Turing" })
-    # print(asdasd)
-    # print("******************")
-    # print(scotts_posts)
-    # # for i in scotts_posts:
-    # #     print(i)
-    # a  = veri1.find_one({'1.spor' : 'futbol'})
-    # b  = veri1.find_one({'1.telefon' : '54564'})
-    # print(a)
-
-    # print("*******b")
     
-    
-    # print(b)
-    
-
-    # csvno1_olanlar = veri1.find({"csvno":"1"})
-    
-    # print(csvno1_olanlar)
-
-    # for i in csvno1_olanlar:
-    #     print(i)
-    
-
-    #kullanici soyisim isterse
-
-
-    # eposta = veri1.find({}, { "eposta":1 })
-    # for i in eposta:
-    #     if len(i) > 1:
-    #         print(i)
-        
-    # csvno0_olanlar = veri1.find({"csvno":"2"})
-    # print(csvno0_olanlar)
-    # for i in csvno0_olanlar:
-    #     print(i)    
-
-    # sporlar = veri1.find({},{"spor":1})
-    # for i in sporlar:
-    #     if len(i)>1:
-    #         print(i)
     tutulanlar= []
     key_son_list = []
     girdi = 0
@@ -117,7 +63,6 @@ def goruntule():
         csvno_girdi = request.form.get("csvno")
         bulunan_anahtarlar = []
         girdi = csvno_girdi
-        print(csvno_girdi)
 
         alinacak_csv = veri1.find({"csvno":csvno_girdi})
         
@@ -125,14 +70,14 @@ def goruntule():
             tutulanlar.append(i)
 
         for i in tutulanlar:
-            # print(" \n" ,i.keys())
+            
             bulunan_anahtarlar.append(i.keys())
         for i in bulunan_anahtarlar:
-            # print(list(i))
+            
             key_son_list.append(list(i))
         k = 1
         for i in list(key_son_list):
-            for j in range(999999): #simdilik
+            for j in range(999999): 
                 try:
                     
                     veriler.append(i[k])
@@ -140,19 +85,14 @@ def goruntule():
 
                     
                 except:
-                    print("An exception occurred")
                     break
-            break            
+            break 
         
 
 
 
         
-    # print(tutulacak.items())
-    # print(len(tutulacak))
-    # for i in tutulanlar:
-    #     print(i)
-        
+    
     return render_template("secim.html",content = veriler,content2 = girdi)
 
 
@@ -161,50 +101,35 @@ def vericek():
     if request.method == 'POST':
         cekilen_bilgiler = []
         verilers = request.form.getlist("checkbox_veriler")
-        for i in verilers:
-            if i !="csvno":
-                geldi = veri1.find({},{i:1,"_id":0}) #veriler cekildi.
 
-                for i in list(geldi):
-                    if(len(i)>0):
-                        # print(i)
-                        cekilen_bilgiler.append(i)
+        csvno_girdi = request.form.get("csvno")
         
-        # for i in cekilen_bilgiler:
-        #     print(i)
+        
+        verilers2 = verilers.copy()
+        
+        
+        verilers2.append("_id")
+        
+        deger = dict.fromkeys(verilers2,1)
+        deger["_id"] = 0
+        
+        geldi = veri1.find({"csvno":csvno_girdi},deger) 
+                
+
+        for i in list(geldi):
+            if(len(i)>0):
+                        
+                 cekilen_bilgiler.append(i)
+        
+        
             
             
 
-        # geldiler = []
-
-        # for i in geldi:
-        #     geldiler.append(i)
-        # print(geldiler)
+       
         yazacagim = []
         for i in list(cekilen_bilgiler):
                 yazacagim.append(list(i.values()))
-        # for i in range(len(yazacagim)):
-        #     print(yazacagim[i])
-        # with open('yeni.csv','w',newline='') as f:
-        #     thewriter = csv.DictWriter(f,verilers)
-        #     thewriter.writeheader()
-
-        #  with open('yeni.csv', 'a') as csvfile:
-        #      spamwriter = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        #      spamwriter.writerow(yazacagim[i])
-              
-
-            #eger verilers[0] ile verilers[1] eşitse aynı türdür pass geç yazdırma. eşit değilse \n koy aşağı yazdırmaya başla.
-        # for i in range(len(yazacagim)):
-        #      for j in range(len(verilers)):
-        #          with open('yeni.csv', 'a') as csvfile:
-        #             if verilers[j-1] != verilers[j]:
-        #                 print("verils1:",verilers[j-1])
-        #                 print("verils2:",verilers[j])
-        #                 spamwriter = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        #                 spamwriter.writerow(yazacagim[i])
-        #                 break
-        #                 print("slas")
+        
 
         file = open('yeni.csv','w',newline='')
         with file:
@@ -227,7 +152,38 @@ def download_file():
     return send_file(p,as_attachment=True)
 
 
+def randomUret():
+    benzersiz = 0
+    
+    csvler = veri1.find({},{"csvno":1,"_id":0})
+    cekilen_csvler = []
+    for i in list(csvler):
+        cekilen_csvler.append(i)
 
+    yazilacak_csv = []
+    for i in list(cekilen_csvler):
+        yazilacak_csv.append(list(i.values()))
+    for i in range(len(yazilacak_csv)):
+        print(yazilacak_csv[i])
+    sart = True
+    if len(yazilacak_csv) == 0 :
+        a = random.randint(1,999999)
+        string_a = str(a)
+        benzersiz = string_a
+        pass
+    else:
+        while(sart):
+            a = random.randint(1,999999)
+            string_a = str(a)
+            for i in range(len(yazilacak_csv)):
+                if list(string_a) == yazilacak_csv[i]:
+                    sart = True
+                    break
+                else:
+                    sart = False
+                    benzersiz = string_a
+
+    return benzersiz
 
 if __name__ == "__main__":
     app.run(debug=True)
